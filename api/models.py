625a2c8540b70e679df6cae6e7106037e846c970
duongnt-96 <duongnt.96@proton.me>
2023-10-30 00:55:15 +0700

    done v0

---
+++ api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    sound = models.CharField(max_length=100)
+
+    def speak(self, *args, **kwargs):
+        return f'The {self.name} says "{self.sound}"'
